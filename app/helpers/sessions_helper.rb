0ce3fbcf638cbf6d22ae6ba2505fd6f80ba2e67b
Rafiqul Islam <mdrafiq261081@gmail.com>
2016-02-15 14:39:13 +0600

    Finish user edit, update, index, and destroy actions

--- app/helpers/sessions_helper.rb
+++ app/helpers/sessions_helper.rb
@@ -13,6 +13,9 @@ def remember(user)
     cookies.permanent.signed[:user_id] = user.id
     cookies.permanent[:remember_token] = user.remember_token
   end
+def current_user?(user)
+    user == current_user
+  end
 
 def current_user
     if (user_id = session[:user_id])
@@ -38,5 +41,14 @@ def forget(user)
     user.forget
     cookies.delete(:user_id)
     cookies.delete(:remember_token)
+  end
+def redirect_back_or(default)
+    redirect_to(session[:forwarding_url] || default)
+    session.delete(:forwarding_url)
+  end
+
+  # Stores the URL trying to be accessed.
+  def store_location
+    session[:forwarding_url] = request.url if request.get?
   end
 end
